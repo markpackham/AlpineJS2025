173ba638dede7239237d6bb24c57992e95bb8c35
M <markpackham1@gmail.com>
2025-02-10 16:45:58 +0000

    Add and call function that logs current count to console
    https://youtu.be/a3Cso8JDUys?si=HwRCy9ahtljBiS_n&t=889

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Trigger as soon as Alphine loads from the html file
 document.addEventListener("alpine:init", () => {
-    Alpine.data('counter', ()=>({
+    Alpine.data('counter', () => ({
         count: 0,
         name: 'Mario',
+
+        logCount() {
+            console.log(this.count)
+        }
     }))
 })
