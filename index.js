cb127e2b10712701810c1e024e52da22b5083a58
M <markpackham1@gmail.com>
2025-02-11 11:49:38 +0000

    Demo data binding via webform
    https://youtu.be/a-hDbPEVogA?feature=shared&t=466

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Trigger as soon as Alphine loads from the html file
 document.addEventListener("alpine:init", () => {
-    Alpine.data('counter', () => ({
-        count: 0,
-        name: 'Mario',
-
-        logCount() {
-            console.log(this.count)
-        }
-    }))
-})
+  Alpine.data("signupForm", () => ({
+    username: "",
+    password: "",
+    passwordConfirm: "",
+    belt: "",
+    bio: "",
+    newsletter: true,
+  }));
+});
