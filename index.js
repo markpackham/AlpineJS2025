eaa62d5e9a2d41b7120571c762fd737c22cde1ac
M <markpackham1@gmail.com>
2025-02-17 14:52:14 +0000

    Clear form is validation successful
    https://youtu.be/TQmqeVRboCE?si=SeGR_OfUxzuDOQGS&t=505

--- index.js
+++ index.js
@@ -34,16 +34,26 @@ document.addEventListener("alpine:init", () => {
       }
     },
 
-    submitForm() {
-      this.validateForm()
+    // We need to get the event to later trigger a form reset
+    // otherwise we'd have to change every single attribute one by one
+    // eg this.username="" this.password=""
+    submitForm($event) {
+      this.validateForm();
+      console.log(this.errors);
 
-      console.log(
-        this.username,
-        this.password,
-        this.belt,
-        this.bio,
-        this.newsletter
-      );
+      // Get an array of keys from the error object
+      if (Object.keys(this.errors).length === 0) {
+        console.log(
+          this.username,
+          this.password,
+          this.belt,
+          this.bio,
+          this.newsletter
+        );
+
+        // Use standard JavaScript reset to clear the form
+        $event.target.reset()
+      }
     },
   }));
 });
