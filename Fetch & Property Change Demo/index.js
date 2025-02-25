cbb534dbf0e6e0df06928138d8556f5d91fac6f4
M <markpackham1@gmail.com>
2025-02-25 12:00:06 +0000

    Finish off background color changer
    https://youtu.be/kALOM4xfdnA?si=Ge1rCxYDMLgnUfQ2&t=477

--- Fetch & Property Change Demo/index.js
+++ Fetch & Property Change Demo/index.js
@@ -11,6 +11,12 @@ document.addEventListener("alpine:init", () => {
       fetch("https://jsonplaceholder.typicode.com/posts?_limit=5")
         .then((response) => response.json())
         .then((data) => (this.posts = data));
+
+      // watch data for changes so page background color changes
+      this.$watch("bg", (newBg, oldBg) => {
+        // Use plain old JS to update the color
+        document.body.style.backgroundColor = newBg;
+      });
     },
   }));
 });
