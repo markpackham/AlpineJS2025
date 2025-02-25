bd5a7f66863e860609b7e33a91f3098372c6f427
M <markpackham1@gmail.com>
2025-02-25 11:49:48 +0000

    Fetch data from jsonplaceholder
    https://youtu.be/kALOM4xfdnA?si=P1qg487URuXF18r2&t=294

--- alpine-crash-course-lesson-11-start/index.js
+++ alpine-crash-course-lesson-11-start/index.js
@@ -1,9 +1,16 @@
-document.addEventListener('alpine:init', () => {
-  Alpine.data('demo', () => ({
+document.addEventListener("alpine:init", () => {
+  Alpine.data("demo", () => ({
     posts: [],
-    bg: '',
+    bg: "",
 
-    
+    // function automatically fires from Alpine.js if page has a "demo" component
+    init() {
+      // console.log('init')
 
+      // fetch data
+      fetch("https://jsonplaceholder.typicode.com/posts?_limit=5")
+        .then((response) => response.json())
+        .then((data) => (this.posts = data));
+    },
   }));
 });
